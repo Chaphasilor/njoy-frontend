be1db62f62b2cf480af5ebf269c556206b04fb7f
Chaphasilor <ppp.u@web.de>
2020-08-15 00:53:07 +0200

    started switching to vuex
    - implemented a basic state for the download dialog
    - moved logic from DirectoryBrowser to PathDialog
    - added ability to create new folders

--- src/components/PathDialog.vue
+++ src/components/PathDialog.vue
@@ -34,7 +34,11 @@
         <DirectoryBrowser
           class="w-full"
           placeholder="Destination Path"
-          v-model="currentPath"
+          :rootDirectoryTree="rootDirectoryTree"
+          :currentDirectory="currentDirectory"
+          :currentPath="currentPath"
+          v-on:directory-down="directoryDown"
+          v-on:directory-up="directoryUp"
         />
 
       </div>
@@ -46,6 +50,7 @@
           class=" w-full h-12"
           type="action"
           label="New Folder"
+          @click.native="createNewDirectory('Test')"
         />    
       </div>
 
@@ -87,14 +92,28 @@ export default {
   data: function() {
     return {
       currentPath: [],
+      currentDirectory: {},
     }
   },
   computed: {
-
+    rootDirectoryTree: function() {
+      return this.$store.getters.rootDirectoryTree;
+    }
   },
   watch: {
     currentPath: function() {
       this.$emit('input', this.currentPath);
+      console.log(`this.currentPath:`, this.currentPath);
+      this.currentDirectory = this.findCurrentDirectory();
+    },
+    invalidPart: function() {
+      if (this.invalidPart.length != 0) {
+        this.currentPath = this.removeInvalidPath(this.currentPath, this.invalidPart);
+        this.invalidPart = [];
+      }
+    },
+    rootDirectoryTree: function() {
+      this.currentDirectory = this.findCurrentDirectory();
     }
   },
   methods: {
@@ -104,9 +123,88 @@ export default {
         return `${pathString}${folderName}/`;
       }, '');
     },
+    findCurrentPathInSubdirectories(currentPath, currentDirectory) {
+      console.log(`currentPath:`, currentPath);
+      console.log(`currentDirectory:`, currentDirectory);
+      if (currentPath.length === 0) {
+        return currentDirectory;
+      } else {
+
+        let foundDir = currentDirectory.subdirectories.filter(dir => dir.name === currentPath[0]);
+        console.log(`foundDir:`, foundDir);
+        if (foundDir.length === 0) {
+          return {
+            exists: false,
+            invalidPart: currentPath,
+            closest: currentDirectory,
+          }
+        } else {
+          return this.findCurrentPathInSubdirectories(currentPath.slice(1), foundDir[0]);
+        }
+        
+      }
+    },
+    findCurrentDirectory() {
+
+      let dir = this.findCurrentPathInSubdirectories(this.currentPath, this.rootDirectoryTree);
+
+      if (dir.exists === false) {
+        console.warn(`The path specified couldn't be found!`);
+        //TODO handle invalid path error
+        alert(`The path specified couldn't be found!`);
+        this.invalidPart = dir.invalidPart;
+        return dir.closest;
+      } else {
+
+        return dir;
+        
+      }
+
+    },
+    findCurrentDirectoryDummy(rootDir) {
+      //TODO remove
+      let dir = this.findCurrentPathInSubdirectories(this.currentPath, rootDir);
+
+      if (dir.exists === false) {
+        console.warn(`The path specified couldn't be found!`);
+        alert(`The path specified couldn't be found!`);
+        this.invalidPart = dir.invalidPart;
+        return dir.closest;
+      } else {
+
+        return dir;
+        
+      }
+
+    },
+    directoryDown(dir) {
+      this.currentPath.push(dir.name);
+    },
+    directoryUp() {
+      this.currentPath.pop();
+    },
+    removeInvalidPath(specifiedPath, invalidPart) {
+      return specifiedPath.slice(0, specifiedPath.length - invalidPart.length);
+    },
     createNewDirectory(name) {
       //TODO create the folder through the API on the server, (then reload all directories)
-      return name;
+      let rootDirectoryTreeCopy = JSON.parse(JSON.stringify(this.rootDirectoryTree));
+
+      let current = this.findCurrentDirectoryDummy(rootDirectoryTreeCopy);
+
+      current.subdirectories.push({
+        name: name,
+        subdirectories: [],
+      })
+      
+      console.log(`current:`, current);
+      console.log(`rootDirectoryTreeCopy:`, rootDirectoryTreeCopy);
+
+      this.$store.dispatch('createNewDummyDirectory', {
+        path: this.currentPath,
+        name: name,
+        newRootDirectoryTree: rootDirectoryTreeCopy,
+      })
     }
   },
   created: function() {
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,165 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    rootDirectoryTree: {
+      name: 'ROOT',
+      subdirectories: [
+        {
+          name: 'Backups',
+          subdirectories: [
+            {
+              name: 'OnePlus 5',
+              subdirectories: [],
+            },
+            {
+              name: 'Raspberry Pi',
+              subdirectories: [],
+            },
+            {
+              name: 'Spectre',
+              subdirectories: [],
+            },
+          ]
+        },
+        {
+          name: 'Documents',
+          subdirectories: [
+            {
+              name: 'Uni',
+              subdirectories: [],
+            },
+            {
+              name: 'Unsorted',
+              subdirectories: [],
+            },
+            {
+              name: 'Work',
+              subdirectories: [],
+            },
+          ]
+        },
+        {
+          name: 'Media',
+          subdirectories: [
+            {
+              name: 'eBooks',
+              subdirectories: [],
+            },
+            {
+              name: 'Movies',
+              subdirectories: [],
+            },
+            {
+              name: 'Music',
+              subdirectories: [],
+            },
+            {
+              name: 'Pictures',
+              subdirectories: [
+                {
+                  name: 'Wallpapers',
+                  subdirectories: [],
+                },
+              ],
+            },
+            {
+              name: 'TV Shows',
+              subdirectories: [
+                {
+                  name: 'Altered Carbon (2018)',
+                  subdirectories: [],
+                },
+                {
+                  name: 'Cloak and Dagger (2018)',
+                  subdirectories: [
+                    {
+                      name: 'S1',
+                      subdirectories: [],
+                    },
+                    {
+                      name: 'S2',
+                      subdirectories: [],
+                    },
+                  ],
+                },
+                {
+                  name: 'Dark (2017)',
+                  subdirectories: [
+                    {
+                      name: 'Season 1',
+                      subdirectories: [],
+                    },
+                    {
+                      name: 'Season 2',
+                      subdirectories: [],
+                    },
+                    {
+                      name: 'Season 3',
+                      subdirectories: [],
+                    },
+                  ],
+                },
+                {
+                  name: 'Dark Matter (2015)',
+                  subdirectories: [
+                    {
+                      name: 'Season 1',
+                      subdirectories: [],
+                    },
+                    {
+                      name: 'Season 2',
+                      subdirectories: [],
+                    },
+                    {
+                      name: 'Season 3',
+                      subdirectories: [],
+                    },
+                  ],
+                },
+              ],
+            },
+          ]
+        },
+        {
+          name: 'Misc',
+          subdirectories: [
+            {
+              name: 'Code',
+              subdirectories: [],
+            },
+            {
+              name: 'Jam Session',
+              subdirectories: [],
+            },
+          ]
+        },
+      ]
+    }
   },
   mutations: {
+    SET_ROOT_DIRECTORY_TREE(state, newRootDirectoryTree) {
+      state.rootDirectoryTree = newRootDirectoryTree;
+    }
   },
   actions: {
+    loadRootDirectoryTree(context) {
+      //TODO fetch from API
+      context.commit('SET_ROOT_DIRECTORY_TREE', context.getters.getRootDirectoryTree);
+    },
+    createNewDirectory(context, path, name) {
+      //TODO create via API
+      console.log(`path:`, path);
+      console.log(`name:`, name);
+      context.actions.loadRootDirectoryTree();
+    },
+    createNewDummyDirectory(context, { path, name, newRootDirectoryTree }) {
+      console.log(`path:`, path);
+      console.log(`name:`, name);
+      console.log(`newRootDirectoryTree:`, newRootDirectoryTree);
+      context.commit('SET_ROOT_DIRECTORY_TREE', newRootDirectoryTree);
+    }
   },
-  modules: {
+  getters: {
+    rootDirectoryTree: state => state.rootDirectoryTree,
   }
 })
