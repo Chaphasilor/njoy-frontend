2a346077d52ed85eb732a1a8f36562fd70582eef
Chaphasilor <ppp.u@web.de>
2020-09-26 19:17:51 +0200

    retry workbox service worker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "@/assets/css/tailwind.css";
-// import './registerServiceWorker'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
