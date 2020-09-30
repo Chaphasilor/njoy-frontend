b75ca3087abd4ffd2ae0319e3fda6a1a535bf788
Chaphasilor <ppp.u@web.de>
2020-09-30 16:34:31 +0200

    fix service worker

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -3,12 +3,13 @@ if (workbox) {
   console.log(`Workbox is active`);
   
   // adjust log level for displaying workbox logs
-  workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
+  workbox.setConfig({
+    debug: true,
+  })
 
-  // apply precaching. In the built version, the precacheManifest will
-  // be imported using importScripts (as is workbox itself) and we can 
-  // precache this. This is all we need for precaching
+  // apply precaching. In the built version, the precacheManifest will be imported using importScripts (as is workbox itself) and we can precache this. This is all we need for precaching
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
 } else {
   console.warn(`Workbox is inactive!`);
 }
